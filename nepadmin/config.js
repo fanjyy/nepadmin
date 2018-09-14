30abf02fd2c3b5835c54d06017e3ad6b10994b1d
fan <504498960@qq.com>
2018-09-14 20:37:44 +0800

    UPDATE

--- nepadmin/config.js
+++ nepadmin/config.js
@@ -35,8 +35,10 @@ layui.define(function (exports) {
         },
         //request 基础URL
         //requestUrl:'http://192.168.0.0/',
-        requestUrl:'./nepadmin/',
-        //requestUrl:'/',
+
+        requestUrl:'./',
+
+        //requestUrl:'./',
         //图片基础URL
         imageUrl:'http://192.168.3.191/',
         //独立页面路由，可随意添加（无需写参数） 前面不能带 /
